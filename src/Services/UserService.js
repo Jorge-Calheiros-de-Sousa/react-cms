bac4c38418d7107d20eb116958122b4923c5ed83
Jorge-Calheiros-de-Sousa <86486202+Jorge-Calheiros-de-Sousa@users.noreply.github.com>
2022-02-24 00:11:47 -0300

    Revert "Feature/adicionando rotas create update users"

--- src/Services/UserService.js
+++ src/Services/UserService.js
@@ -10,23 +10,6 @@ class UserService {
             return [null, error]
         }
     }
-
-    async create(data) {
-        try {
-            const response = await Api.post("/api/users", data);
-            return [response.data, false];
-        } catch (error) {
-            return [null, error];
-        }
-    }
-    async update(data, id) {
-        try {
-            const response = await Api.put(`/api/users/${id}`, data);
-            return [response.data, false];
-        } catch (error) {
-            return [null, error];
-        }
-    }
 }
 
 export default new UserService();
